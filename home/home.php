abfd757eb6be7ed06cb18e21555362dfe208c0e4
luaXoXo <79286488+luaXoXo@users.noreply.github.com>
2024-12-04 14:52:18 -0300

    home v1

---
+++ home/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<link rel="stylesheet" href="estilo.css">
+<body>
+    <header>
+        <img src="" alt="Logo">
+        <nav>
+            <a href="#">Conta</a>
+            <a href="#">Anuncie</a>
+        </nav>
+    </header>
+    <div class="search-bar">
+        <input type="text" placeholder="O que voce deseja">
+        <button>Buscar</button>
+    </div>
+    <section class="categories">
+        <div class="category">Mulheres</div>
+        <div class="category">Homens</div>
+        <div class="category">Infantil</div>
+        <div class="category">Diversos</div>
+    </section>
+    <section class="listings">
+        <div class="listing">
+            <img src="imagens/calca-cargo.png" alt="Calça cargo">
+            <div class="info">
+                <h3>Calça cargo</h3>
+                <p>vendedor: ShopInc.</p>
+                <p>R$100</p>
+            </div>
+        </div>
+        <div class="listing">
+            <img src="imagens/canisaOversize.png" alt="Camisa Oversize">
+            <div class="info">
+                <h3>Camiseta Oversize</h3>
+                <p>vendedor: FIGconfections</p>
+                <p>R$78</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
